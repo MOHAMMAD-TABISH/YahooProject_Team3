f0365d8221d9cdeffe5e8ee64009a54287664a4e
Mohammad Tabish <1000071044@hexaware.com>
2022-06-29 12:28:14 +0530

    Commit

---
+++ Models/AccountModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YahooProject_Team3.Models
+{
+    public class Login
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
